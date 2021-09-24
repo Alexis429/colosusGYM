dca2721e54525f5e0aa85cf7d82c383d99f0b538
1NE0 <juan.ortiz.alzate@correounivalle.edu.co>
2021-09-24 17:50:55 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { app_routing } from './app.route';
+
+// routes
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.route.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+const app_routes: Routes = [
+    {path: '' , component: HomeComponent},
+    {path: 'login' , component: LoginComponent},
+    {path: '**' , pathMatch: 'full' , redirectTo: ''}
+];
+
+export const app_routing = RouterModule.forRoot(app_routes);
